c41ea533e98f54aad37f2e4d773095c7eaa3cb2e
Viktoriia <mifasollasidorem@gmail.com>
2014-12-09 21:58:54 +0200

    added project files

---
+++ diploma_project_1/diploma_project_1/Graphs/GreedyAlg/VertexPriority.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace diploma_project_1.Graphs.GreedyAlg {
+
+    class VertexPriority : IComparable<VertexPriority> {
+
+        private int vertexNumber;
+        private int vertexPriority;
+
+        public VertexPriority(int vertex, int priority) {
+            vertexNumber = vertex;
+            vertexPriority = priority;
+        }
+
+
+        public int CompareTo(VertexPriority obj) {
+            if (vertexPriority < obj.vertexPriority)
+                return 1;
+            else if (vertexPriority == obj.vertexPriority)
+                return 0;
+            
+            return -1;
+        }
+
+       
+    }
+
+}
